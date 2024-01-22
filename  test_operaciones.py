22367f6fea47ee78265a4ea1770a09edd6d093a6
Sofia Fernandes <123645624+sofiafernands@users.noreply.github.com>
2024-01-22 21:21:16 +0000

    archivos de python subidos

---
+++  test_operaciones.py
@@ -0,0 +1,31 @@
+# test_operaciones.py
+
+import unittest
+from suma import sumar, restar, multiplicar, dividir
+
+class TestOperaciones(unittest.TestCase):
+    def test_sumar(self):
+        self.assertEqual(sumar(3, 2), 5)
+        self.assertEqual(sumar(-1, 1), 0)
+        self.assertEqual(sumar(-1, -1), -2)
+
+    def test_restar(self):
+        self.assertEqual(restar(3, 2), 1)
+        self.assertEqual(restar(-1, 1), -2)
+        self.assertEqual(restar(-1, -1), 0)
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(-1, 1), -1)
+        self.assertEqual(multiplicar(-1, -1), 1)
+
+    def test_dividir(self):
+        self.assertEqual(dividir(6, 2), 3)
+        self.assertEqual(dividir(-6, 2), -3)
+        self.assertEqual(dividir(0, 5), 0)
+
+        with self.assertRaises(ValueError):
+            dividir(1, 0)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ suma.py
@@ -0,0 +1,18 @@
+# suma.py
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir por cero.")
+    return a / b
+
+if __name__ == "__main__":
+    print(sumar(5, 3))
